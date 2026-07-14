4e843c026060047fdc4068986ac77b8cfd0a2e6e
K_Anastasia <kindeevaanastasia68@gmail.com>
2026-07-14 20:19:09 +0300

    Додано приклад додавання A + B

--- test_Git_repository/Program.cs
+++ test_Git_repository/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+Console.Write("Введіть число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введіть число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int sum = A + B;
+
+Console.WriteLine("Сума " + sum);
+
+Console.WriteLine("Натисніть будь-яку клавішу для виходу...");
+Console.ReadKey();
